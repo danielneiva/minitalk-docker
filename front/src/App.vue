edf48aa19f470f6b9fc97a9416a3a98ed1c4a03d
Gustavo Loschi <gustavo@onfly.com.br>
2024-01-16 18:39:32 -0300

    Aplicação de lista

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,47 +1,52 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <p style="font-size: 50px; font-weight: bold">Bem vindo ao cadastro de texto!</p>
   </header>
 
   <main>
-    <TheWelcome />
+      <form class="search-form">
+          <div style="display: flex; flex-direction: row">
+              <input id="text" type="text" style="height: 50px; font-size: 32px">
+              <input type="button"  value="Inserir" style="margin-left: 10px" @click="insertItem()">
+          </div>
+      </form>
+      <div style="width: 500px; padding: 1px; background-color: grey; margin: 30px 0"></div>
+      <div style="display: flex; flex-direction: column">
+          <div v-for="(item, index) in items" class="card-item">
+              <p>{{item}}</p>
+              <input type="button" value="Excluir" @click="removeItem(index)">
+          </div>
+      </div>
   </main>
 </template>
 
+<script setup>
+import {ref} from "vue";
+
+const items = ref([]);
+
+function insertItem() {
+    items.value.push(document.getElementById("text").value);
+}
+
+function removeItem(index) {
+    items.value.splice(index, 1);
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
+.card-item {
+    background-color: #BBB;
+    border: 1px solid #555;
+    color: #333;
+    padding: 20px;
+    margin-bottom: 20px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    width: 500px;
+    font-size: 20px;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    flex-direction: row;
+    justify-content: space-between;
 }
 </style>
